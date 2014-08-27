1d9f06a814288a10b059d45393abd158c8037df9
eprv <web@eprudnikov.com>
2014-08-27 11:29:15 +0400

    Update header.php

--- header.php
+++ header.php
@@ -1,11 +1,4 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
-
-<html>
-<head>
-<!-- test -->
 <?php
-
  session_start(); 
   $data_array = @file('cn.txt');
   $server=rtrim($data_array[0]);
@@ -13,110 +6,126 @@
   $loginDB=rtrim($data_array[2]);
   $passwordDB=rtrim($data_array[3]);
 
-    if (!mysql_connect($server, $loginDB, $passwordDB)) {
-	echo "Could not connect to DB";
-	exit;
+  if (!mysql_connect($server, $loginDB, $passwordDB)) {
+    die("Could not connect to DB");
 	}	
-    mysql_select_DB($DB) or exit("Could not select DB");
-
-
-echo '<title>'.$GLOBALS["seoTitle"].'</title>';  
-echo '<meta name="keywords" content="'.$GLOBALS["seoKeywords"].'"/>';
-echo '<meta name="description" content="'.$GLOBALS["seoDescription"].'"/>';
+  mysql_select_DB($DB) or exit("Could not select DB");
 ?>
-  <META http-equiv=Content-Type content="text/html; charset=windows-1251">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+
+<?php
+  echo '<title>'.$GLOBALS["seoTitle"].'</title>';  
+  echo '<meta name="keywords" content="'.$GLOBALS["seoKeywords"].'"/>';
+  echo '<meta name="description" content="'.$GLOBALS["seoDescription"].'"/>';
+?>
+  <meta http-equiv=Content-Type content="text/html; charset=windows-1251">
   <meta name='yandex-verification' content='5902a294fe8b0de1' />
-   <link rel="icon" href="favicon.ico" type="image/x-icon">
-   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"> 
   <LINK type="text/css" rel="StyleSheet" href="style.css">
+  <link type="text/css"  rel="stylesheet" href="smslider/smslider.css" />
+  <script type="text/javascript" src="http://yandex.st/jquery/2.1.1/jquery.min.js"></script>
+  <script type="text/javascript" src="smslider/jquery.smslider.min.js"></script>
+  <script>
+  $(document).ready(function(){
+      $('#slider').smSlider({
+        transition : 'animate',
+        autoPlay : true,
+        subMenu : true,
+        subMenuClass : 'sm_submenu-item',
+        pagination : false
+
+      });
+  });
+  </script>
 </head>
 
-<body topmargin="3" leftmargin="2" rightmargin="2" background="image/background.jpg">
+<body>
 
-
-
-<TABLE width="100%" border="0" cellpadding="0" cellspacing="0" >
-
-<TR> <td width="100%" colspan="3" align="left" height="25" background="image/green_yellow.jpg" style="font-size:12px">
-<?php
-echo $GLOBALS["seoHeaderText"];
-?>
-</td> 
-</TR>
-
-<TR> 
-<td width="240" valign="top" align="left" height="200"> <a href="/"><img border="0" src="image/logo.jpg" alt="Пиломатериалы: доска, брус, брусок" title="Главная"></a> </td> 
-
-<td width="100%" valign="top" align="center" height="200"  >
-<table valign="top" height="200" width="100%" border="0" cellpadding="0" cellspacing="0" background="image/top200.jpg">
-<tr>
-<td width="50%" valign="top" align="left" height="200" > <img valign="top" height="200" src="image/top200.jpg"></td>
-<td width="580" valign="top" align="center" height="200" > <img valign="top" align="center" width="580" height="255" title="Деревянные окна и двери, доска, брус" alt="Деревянные окна и двери, доска, брус" src="image/drevstroy.jpg"></td>
-<td  width="50%" valign="top" align="right" height="200" > <img valign="top" height="200" src="image/top200.jpg"></td>
-</tr>
+<TABLE width="100%" border="0" cellpadding="0" cellspacing="0" class="top">
+  <TR>
+    <td width="100%" colspan="3" align="left" height="25" background="image/green_yellow.jpg" style="font-size:12px">
+    <?php echo $GLOBALS["seoHeaderText"];?>
+    </td>
+  </TR>
 </table>
-</td>	
 
-<td width="180" valign="top" align="right"  height="200"> <img width="180" height="200" src="image/center_4.jpg" ></td> 	
+<div class="header">
+  <div class="logo">
+    <a href="./"><img src="image/logo.png" title="" alt=""></a>
+  </div>
+  <div class="span1">
+    <div class="name">База строительных материалов</div>
+  </div>
+  <div class="span1 phones">
+    Звоните сейчас<br><strong style="color:#F00530">8 (473) 229-67-40</strong><br>8 (920) 211- 67- 40
+  </div>
+  <div class="span1 address">
+    Адрес: <strong>г. Воронеж,<br>ул. Тихий Дон, 57<br><a href="./contact.php#map">карта проезда</a></strong>
+  </div>
+</div>
 
-</TR>
-</TABLE>
+<div class="pl-slider">
+  <div id="slider">
+    <ul>
+      <li><img src="image/drevstroy.jpg"></li>
+      <li><img src="image/slide-2.jpg"></li>
+      <li><img src="image/slide-3.jpg"></li>
+      <li><img src="image/slide-1.jpg"></li>
+    </ul>
+  </div>
+  <div class="slidermenu">
+    <ul>
+      <li><a class="sm_submenu-item" href="#" data-index="0">Пиломатериалы</a></li>
+      <li><a class="sm_submenu-item" href="#" data-index="1">Крепёж</a></li>
+      <li><a class="sm_submenu-item" href="#" data-index="2">Утеплители</a></li>
+      <li><a class="sm_submenu-item" href="#" data-index="3">Доставка</a></li>
+    </ul>
+  </div>
+</div>
 
 <TABLE width="100%" border="0" cellpadding="0" cellspacing="0">
-<TR><td align="center" colspan="3" align="left"  height="25" background="image/green_yellow.jpg"><strong>Лучший дом себе построй - приезжайте к нам в Древострой! </strong></td></TR>
-
 <TR>
-	<td align="left" width="240" valign="top">
-	
-          <br>
-           <table border="1" width="240" bordercolor="#996633">
-             	<tr>
-               	   <td width="100%">
-                      <table border="0" cellpadding="0" cellspacing="0" width="100%" background="image/green_yellow.jpg" >
-                         <tr>
-                           <td class="menuheader" >Меню</td>
-			  </tr>
-                      </table>                 
-		    </td>
-                </tr> 
-              
-                <tr>
-                   <td width="100%">
-                      <table border="0" bordercolor="#47734C" width="100%"  bordercolordark="#47734C">
-                         <tr>
-                            <td width="100%" >
-                               <table border="0" cellpadding="2" cellspacing="3" width="100%">
-				  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="/">Главная</a></td></tr>       
-				  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="doski_v_Voronezhe.php">Доска обрезная</a></td></tr>  
-				  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="brus_v_Voronezhe.php">Брус</a></td></tr>    
-                                  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="sushka_drevesiny.php">Сушка древесины</a></td></tr> 
-                                  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="price_v_Voronezhe.php">Цены на пиломатериалы</a></td></tr>
-                                  <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="photo.php">Фотогалерея</a></td></tr>  
-			          <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="contact.php">Контакты</a></td></tr>                      
-                               </table>  
-                            </td>
-                         </tr>
-                      </table> 
-					</td>
-				</tr>	
-				<tr>
-					<td>
-					
-					<img src="./image/banner.gif">
-					</img>
-					
-					</td>					
-                   </td>
-                </tr>
-               
+<td align="left" width="240" valign="top">
+<table border="1" width="240" bordercolor="#996633">
+<tr>
+  <td width="100%">
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" background="image/green_yellow.jpg" >
+      <tr><td class="menuheader" >Меню</td></tr>
+    </table>                 
+  </td>
+</tr> 
 
-             </table>
-	<br>
+<tr>
+<td width="100%">
+<table border="0" bordercolor="#47734C" width="100%"  bordercolordark="#47734C">
+  <tr>
+    <td width="100%" >
+    <table border="0" cellpadding="2" cellspacing="3" width="100%">
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="/">Главная</a></td></tr>       
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="doski_v_Voronezhe.php">Доска обрезная</a></td></tr>  
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="brus_v_Voronezhe.php">Брус</a></td></tr>    
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="sushka_drevesiny.php">Сушка древесины</a></td></tr> 
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="price_v_Voronezhe.php">Цены на пиломатериалы</a></td></tr>
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="photo.php">Фотогалерея</a></td></tr>  
+      <tr><td class="menuitems" background="image/menuitem.jpg"> <a href="contact.php">Контакты</a></td></tr>                      
+    </table>
+    </td>
+  </tr>
+  </table> 
+  </td>
+  </tr>	
+  <tr>
+  <td>
 
-	  
-	</td>
+  <img src="./image/banner.gif">
+  </img>
 
-	
-	
-	<td valign="top" align="center" height="900">
-	<br>
+  </td>					
+  </tr>
+</table>
+</td>
+<td valign="top" align="center" height="900">
